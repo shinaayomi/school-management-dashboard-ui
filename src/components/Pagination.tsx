2c2e4f21524766010bcd9ad81b2158e9a13035d7
shinaayomi <lateef718@gmail.com>
2024-10-13 14:43:27 +0100

    pagination ui

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -9,6 +9,16 @@ export default function Pagination() {
       >
         Prev
       </button>
+      <div className="flex items-center gap-2 text-sm">
+        <button className="px-2 rounded-sm bg-shinaSky">1</button>
+        <button className="px-2 rounded-sm">2</button>
+        <button className="px-2 rounded-sm">3</button>
+        <span>...</span>
+        <button className="px-2 rounded-sm">10</button>
+      </div>
+      <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Next
+      </button>
     </div>
   );
 }
