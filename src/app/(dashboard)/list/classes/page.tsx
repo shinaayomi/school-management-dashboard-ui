a3279a6353e4997249187b36e796a389a3ce3c30
shinaayomi <lateef718@gmail.com>
2024-12-02 14:03:19 +0100

    classes

---
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -0,0 +1,96 @@
+import FormModal from "@/components/FormModal";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { classesData, role } from "@/lib/data";
+import Image from "next/image";
+import React from "react";
+
+type Class = {
+  id: number;
+  name: string;
+  capacity: number;
+  grade: number;
+  supervisor: string;
+};
+
+const columns = [
+  { header: "Class Name", accessor: "name" },
+  {
+    header: "Capacity",
+    accessor: "capacity",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
+    className: "hidden md:table-cell",
+  },
+  { header: "Actions", accessor: "actions" },
+];
+
+export default function ClassListPage() {
+  const renderRow = (item: Class) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:border-slate-50 text-sm hover:bg-shinaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+      <td className="hidden md:table-cell">{item.capacity}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.supervisor}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" && (
+            <>
+              <FormModal table="class" type="update" data={item} />
+              <FormModal table="class" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="bg-white p-4 rounded-md m-4 mt-0">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-shinaYellow">
+              <Image
+                src="/images/filter.png"
+                alt="filter"
+                width={14}
+                height={14}
+                priority
+              />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-shinaYellow">
+              <Image
+                src="/images/sort.png"
+                alt="filter"
+                width={14}
+                height={14}
+                priority
+              />
+            </button>
+            {role === "admin" && <FormModal table="class" type="create" />}
+          </div>
+        </div>
+      </div>
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      {/* PAGINATION */}
+      <Pagination />
+    </div>
+  );
+}
