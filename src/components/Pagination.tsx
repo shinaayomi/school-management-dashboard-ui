cc2381ecd4e92b7b5ecc0ef132155b89e92d8f86
shinaayomi <lateef718@gmail.com>
2025-05-15 08:29:09 +0100

    pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,22 +1,66 @@
+"use client";
+
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function Pagination() {
+export default function Pagination({
+  page,
+  count,
+}: {
+  page: number;
+  count: number;
+}) {
+  const router = useRouter();
+
+  const hasPrev = ITEM_PER_PAGE * (page - 1) > 0;
+  const hasNext = ITEM_PER_PAGE * page < count;
+
+  const changePage = (newPage: number) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set("page", newPage.toString());
+    router.push(`${window.location.pathname}?${params}`);
+  };
+
   return (
     <div className="p-4 flex items-center justify-between text-gray-500">
       <button
-        disabled
+        disabled={!hasPrev}
         className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+        onClick={() => changePage(page - 1)}
       >
         Prev
       </button>
       <div className="flex items-center gap-2 text-sm">
-        <button className="px-2 rounded-sm bg-shinaSky">1</button>
+        {Array.from(
+          { length: Math.ceil(count / ITEM_PER_PAGE) },
+          (_, index) => {
+            const pageIndex = index + 1;
+            return (
+              <button
+                key={pageIndex}
+                className={`px-2 rounded-sm ${
+                  pageIndex === page ? "bg-shinaSky" : "hover:bg-shinaSkyLight"
+                }`}
+                onClick={() => changePage(pageIndex)}
+              >
+                {pageIndex}
+              </button>
+            );
+          }
+        )}
+
+        {/* <button className="px-2 rounded-sm bg-shinaSky">1</button>
         <button className="px-2 rounded-sm">2</button>
         <button className="px-2 rounded-sm">3</button>
         <span>...</span>
-        <button className="px-2 rounded-sm">10</button>
+        <button className="px-2 rounded-sm">10</button> */}
       </div>
-      <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+      <button
+        disabled={!hasNext}
+        className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+        onClick={() => changePage(page + 1)}
+      >
         Next
       </button>
     </div>
