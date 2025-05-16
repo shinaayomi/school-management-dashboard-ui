328feba69b8a93da14805ee4b5fe27795368b6c9
shinaayomi <lateef718@gmail.com>
2025-05-16 22:13:22 +0100

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Custom rule override to allow `any`
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      // "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
