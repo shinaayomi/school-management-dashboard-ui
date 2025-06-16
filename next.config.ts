6ceb778891a15baa26c269b2a98a60f6d80472d3
shinaayomi <lateef718@gmail.com>
2025-06-16 08:39:46 +0100

    next.config

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    appIsrStatus: false,
-    // buildActivity: false,
-  },
+
+  // devIndicators: {
+  //   appIsrStatus: false,
+  //   // buildActivity: false,
+  // },
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
